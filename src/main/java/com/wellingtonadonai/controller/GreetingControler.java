9469949070696475974023e886970b053f6c8dd5
wellington <wellington.h.s.oliveira@hotmail.com>
2025-05-16 10:48:26 -0300

    Primeiro Commit

---
+++ src/main/java/com/wellingtonadonai/controller/GreetingControler.java
@@ -0,0 +1,27 @@
+package com.wellingtonadonai.controller;
+
+import com.wellingtonadonai.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingControler {
+
+        private static final String template = "Hello World, %s!";
+        private final AtomicLong counter = new AtomicLong();
+
+        @RequestMapping("/greeting")
+        public Greeting greeting(
+                @RequestParam(value = "name" , defaultValue = "World")
+                String name){
+
+            return new Greeting (counter.incrementAndGet(), String.format(template, name));
+
+        }
+    }
+
+
+
---
+++ src/main/java/com/wellingtonadonai/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.wellingtonadonai.model;
+
+public record Greeting(long id , String content) {
+
+}
